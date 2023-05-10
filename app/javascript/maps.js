0beab46e87710903bb90e83cb36a32b65ef977e2
Henry Baisch <henrybaisch02@gmail.com>
2023-05-09 17:39:00 -0700

    Maps more

---
+++ app/javascript/maps.js
@@ -0,0 +1,6 @@
+var mapOptions = {
+  center: { lat: 37.7749, lng: -122.4194 },
+  zoom: 10,
+  mapId: 'b4afdd511368d372'
+};
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
