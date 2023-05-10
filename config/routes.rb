bb508d4d1d81b57c4e352372e81731c4e409b531
Henry Baisch <henrybaisch02@gmail.com>
2023-05-09 21:04:07 -0700

    Create User Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'user_home', to: 'users#home'
+
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'posts#index'
