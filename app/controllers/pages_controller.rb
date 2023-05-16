21b94e7fbc7f81082cfa700371936d9ad3d0cb90
Henry Baisch <henrybaisch02@gmail.com>
2023-05-15 21:39:31 -0700

    Begin editing posts index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
-  def home
+  before_action :set_post, only: %i[ show edit update destroy ]
+
+
+  def index
+    @posts = Post.all
+  end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :description, :artist, :genre, :date, :time, :location, :address)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'user_home', to: 'users#home'
-
+  get 'discover', to: 'pages#discover'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'posts#index'
