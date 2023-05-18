4e84a2f43bbbae3aeeec0bc81e1037085fbd4817
Henry Baisch <henrybaisch02@gmail.com>
2023-05-18 16:36:21 -0700

    Add buttons to navbar top

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :artists
   devise_for :users
-  get 'user_home', to: 'users#home'
+  get 'profile', to: 'users#home'
   get 'discover', to: 'pages#discover'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
