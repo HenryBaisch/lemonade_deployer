b5c6a07e54b3b57490bae17d1c19ecadb62acb9e
Henry Baisch <henrybaisch02@gmail.com>
2023-05-18 10:55:32 -0700

    Add User attribute to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
     @post.image.attach(params[:post][:image]) # attach the uploaded image to the post
     respond_to do |format|
       if @post.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_16_182809) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_18_174653) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_182809) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -75,4 +77,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_182809) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
